de269c3148c6dc7f634de4889b655f41ee042a73
Andres Ceccoli <andres.ceccoli@sitrack.com>
2020-10-16 10:15:22 -0300

    button

--- src/LineCrud.jsx
+++ src/LineCrud.jsx
@@ -1,28 +1,39 @@
 import React from "react";
-import {Row, Table, Col} from "reactstrap";
+import {Row, Table, Col, Button} from "reactstrap";
 
 const LineCrud = () => {
-  
+
 
   return (
-      <Row>
-        <Col xs={12} md={5}>
-          <Table>
-            <thead>
-              <tr>
-                <th>Key</th>
-                <th>Color</th>
-                <th className="d-none d-md-table-cell">
-                  Stroke
-                </th>
-                <th>
-                  Order
-                </th>
-              </tr>
-            </thead>
-          </Table>
-        </Col>
-      </Row>
+      <div className="m-3">
+        <Row className="mb-2">
+          <Col xs={12} className="d-flex justify-content-end">
+            <Button color="primary">Add</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} md={5}>
+            <Table>
+              <thead>
+                <tr>
+                  <th>Key</th>
+                  <th>Color</th>
+                  <th className="d-none d-md-table-cell">
+                    Stroke
+                  </th>
+                  <th>
+                    Order
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                </tr>
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </div>
   );
 };
 
